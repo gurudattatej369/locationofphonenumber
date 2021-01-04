dbb697c5be3bb3842b52a63ae57b460c3beba252
gurudattatej369 <68219300+gurudattatej369@users.noreply.github.com>
2021-01-04 22:18:23 +0530

    Create numberlocation.py
    this is the python code to find the location of a phone number

---
+++ numberlocation.py
@@ -0,0 +1,12 @@
+import phonenumbers
+number=input("Enter your phone number with country code:")
+
+from phonenumbers import geocoder
+
+ch_nmber=phonenumbers.parse(number,"CH")
+print(geocoder.description_for_number(ch_nmber,"en"))
+
+from phonenumbers import carrier
+
+service_nmber =phonenumbers.parse(number,"RO")
+print(carrier.name_for_number(service_nmber,"en"))
